c47750530a10311857fcaae61adf1077093dbb1b
Michal Markus <michal@markus-it.cz>
2025-01-26 23:16:26 +0100

    Added splash-screen settings,

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'cz.michalmarkus.photogalleryexam',
   appName: 'Photo Gallery Exam',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    SplashScreen: {
+      laungShowDuration: 5000,
+      launchAutoHide: true,
+      backgroudColor: '#8EFFD5',
+    }
+  }
 };
 
 export default config;
