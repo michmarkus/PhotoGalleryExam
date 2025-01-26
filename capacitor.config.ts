29bb35bfb889a1f50cba4d0d9f853eb885cb9af9
Michal Markus <michal@markus-it.cz>
2025-01-26 21:44:41 +0100

    New icon resources and appId change

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'cz.michalmarkus.photogalleryexam',
   appName: 'Photo Gallery Exam',
   webDir: 'dist'
 };
